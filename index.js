625eb0873519975cff4b712446407c982904e38d
mjs-dinesh <dineshmjs22@gmail.com>
2022-01-27 21:02:28 +0530

    mkj

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ console.log("Github Webhooks");
 const name = "Dinesh";
 console.log(`Hi ${name}`);
 console.log("hook api test...");
-console.log("code changes.......")
+console.log("code changes.s......")
